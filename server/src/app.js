2909a3a7dfc65a6a9c97300303044057a4eb3e10
Alejandro Fraga <alex.fraga@gmail.com>
2018-08-19 09:14:20 -0400

    Adding initial server code

---
+++ server/src/app.js
@@ -0,0 +1,26 @@
+// app-server v 1.0
+// The modules to be used are:
+// - express: to enable the web server
+const express = require('express');
+// -body-parser: to easily  JSON parsing
+const bodyParser = require('body-parser');
+// - cors: allows any server hit our server (* risk as any server can reach us)
+const cors = require('cors');
+// - morgan: used for logging/tracing purposes
+const morgan = require('morgan');
+
+// Defining express app
+const app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors());
+
+//Enabling end point
+app.get('/status', (req,res) => {
+  res.send({
+    message: "Hello World... as ususal!"
+  })
+});
+
+// Enabling listening
+app.listen(process.env.PORT || 8081);
